072439ed07cd1fac2398601c8fe185ed220714ab
Eric Maxwell <emaxwell@gmail.com>
2015-07-23 21:58:48 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions work together to make it faster to retreive
+## the solve reuslt of a matrix if the solution is needed more than once.
+## Pass a matrix to the makeCacheMatrix and it returns a special matrix
+## that can then be used with the cacheSolve function.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes a matrix as input. It stores the input matrix 
+## within its scope and returns a list of functions for retrieving and 
+## manipulating the matrix and its cached result.
+## $set is used to set or reset the matrix
+## $get is used to retreive the matrix
+## $setsolve is used to store(cache) the solved matrix
+## $getsolve is used to retreive the solved matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
@@ -18,7 +27,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the special matrix as input and first checks to see
+## if the matrix solution already exists.If it does then the cached result 
+## is retreived and returned.  Else if the solution does not already exist
+## then the matrix is solved and stored in the special matrix and then
+## returns the result.
 
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
